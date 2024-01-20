c2914c681af98c8bceb5e8eefe06b13079a4d6f1
Lydell Dsouza <lydelldsouza24@gmail.com>
2024-01-20 22:14:26 +0530

    usermodel

--- user.models.js
+++ user.models.js
@@ -6,17 +6,33 @@ const UserSchema = new mongoose.Schema(
       required: true,
       unique: true,
       lowercase: true,
+      trim:true,
+      index:true
     },
     EmailId: {
       type: String,
       required: true,
       lowercase: true,
+      unique: true,
+      trim:true,
+      index:true
+    },
+    avatar:{
+      type:String, //cloudinary//
     },
     password: {
       type: String,
       required: [true, 'password is required'],
     },
+    phoneNumber: {
+      type:int,
+      required:true
+    },
+    refreshToken:{
+      type:String
+    },
   },
+ 
   { timestamps: true }
 );
 
